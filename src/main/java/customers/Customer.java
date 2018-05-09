19b98a9fdd39f7cada5a8016d36c495f4dfe0ff2
upuld <u.dissanayaka@gmail.com>
2018-05-09 13:20:31 +0100

    adds customer loyalty card status

--- src/main/java/customers/Customer.java
+++ src/main/java/customers/Customer.java
@@ -1,4 +1,16 @@
 package customers;
 
+import java.util.ArrayList;
+
 public class Customer {
+
+    private ArrayList<LoyaltyCard> loyaltyCard;
+
+    public Customer() {
+        this.loyaltyCard = new ArrayList<>();
+    }
+
+    public boolean hasLoyaltyCard() {
+        return (loyaltyCard.size() > 0);
+    }
 }
--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,2 +1,23 @@
+import customers.Customer;
+import customers.LoyaltyCard;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
+    LoyaltyCard loyaltyCard;
+    Customer customer;
+
+    @Before
+    public void before() {
+        loyaltyCard = new LoyaltyCard();
+        customer = new Customer();
+    }
+
+    @Test
+    public void startsWithoutLoyaltyCard() {
+        assertEquals(false, customer.hasLoyaltyCard());
+    }
 }
