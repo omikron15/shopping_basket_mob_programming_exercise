6e68760a1309a0b0086188e6b83cfab42339c6db
upuld <u.dissanayaka@gmail.com>
2018-05-09 14:27:03 +0100

    Adds basket get total

--- src/main/java/shop/Basket.java
+++ src/main/java/shop/Basket.java
@@ -1,4 +1,20 @@
 package shop;
 
+import products.Product;
+
+import java.util.ArrayList;
+
 public class Basket {
+
+    private double total;
+    private ArrayList<Product> products;
+
+    public Basket() {
+        this.total = 0.0;
+        this.products = new ArrayList<>();
+    }
+
+    public double getTotal() {
+        return total;
+    }
 }
--- src/test/java/BasketTest.java
+++ src/test/java/BasketTest.java
@@ -1,2 +1,29 @@
+import customers.Customer;
+import customers.LoyaltyCard;
+import org.junit.Before;
+import org.junit.Test;
+import products.Food;
+import shop.Basket;
+
+import static org.junit.Assert.assertEquals;
+
 public class BasketTest {
+
+    Basket basket;
+    Food food;
+    LoyaltyCard loyaltyCard;
+    Customer customer;
+
+    @Before
+    public void before() {
+        loyaltyCard = new LoyaltyCard();
+        customer = new Customer();
+        food = new Food("Doughnut", 0.80);
+        basket = new Basket();
+    }
+
+    @Test
+    public void totalStartsAt0() {
+        assertEquals(0.0, basket.getTotal(), 0.01);
+    }
 }
